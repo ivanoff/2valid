8e8b6ac58e9471cfecfc304fd5f4e2087cd0dccd
d-i-m-a <my.name.is.dimitry@gmail.com>
2015-10-14 15:28:19 +0300

    update tests

--- test/index.js
+++ test/index.js
@@ -1,93 +1,124 @@
-var should = require('chai').should(),
-    vm = require('../index'),
-    errors = vm.errors,
-    show = vm.showModels,
-    showExpanded = vm.showModelsExpanded,
-    validate = vm.validate,
-    dispose = vm.dispose,
-    showErrors = vm.showErrors;
+'use strict';
 
-describe('#register', function() {
+var should = require('chai').should();
 
-  it('register new model', function() {
-    vm.registerModel( "user", {
-        id:   { type: "uuid", required: true },
-        name: { 
-            first : { type: "string", min: 1, max: 256, required: true },
-            last  : { type: "string", min: 1, max: 256 },
-        },
-        email: { type: "email" },
-        metadata: { type: "object" },
-    }).should.be.false;
-  });
+describe('Validate-me tests', function () {
 
+    beforeEach(function () {
+        this.vm = require('../index')
+    })
+    afterEach(function () {
+        this.vm = null
+    })
 
-  it('check good data', function() {
-    vm.validate( "user", {
-      id    : "61cecfb4-da43-4b65-aaa0-f1c3be81ec53",
-      name  : { first : "Alex", last: "Bagdanov", },
-      metadata: { tt1:1, tt2:2 },
-    }).should.equal(false);
-  });    
+    describe('Model to validate nested and required data', function () {
+
+        it('register new model', function() {
+            this.vm.registerModel( "user", {
+                id:   { type: "uuid", required: true },
+                name: { 
+                    first : { type: "string", min: 1, max: 256, required: true },
+                    last  : { type: "string", min: 1, max: 256 },
+                },
+                email: { type: "email" },
+                metadata: { type: "object" },
+            }).should.be.false;
+        });
 
 
-  it('register model with match', function() {
-    vm.registerModel( "user_match", {
-        name: { type: "string", match : /^[A-Z]+$/ }
-    }).should.be.false;
-  });
+        it('check good data', function() {
+            this.vm.validate( "user", {
+                id    : "61cecfb4-da43-4b65-aaa0-f1c3be81ec53",
+                name  : { first : "Alex", last: "Bagdanov", },
+                metadata: { tt1:1, tt2:2 },
+            }).should.equal(false);
+        });    
+
+        it('check bad data', function() {
+            this.vm.validate( "user", {
+                id    : "61cecfb4-da43-4b65-aaa0-f1c3be81ec53",
+                name  : { last: "Bagdanov", },
+                metadata: { tt1:1, tt2:2 },
+                createdAt : new Date(),
+            }).should.eql({ '': 'Field "createdAt" not found in registered model',
+                ',name': 'Field "first" is requied, but not found' });
+        });    
+    });
 
 
-  it('check match data', function() {
-    vm.validate( "user_match", 
-        { name  : "ILIKECAPS" }
-    ).should.equal(false);
-  });    
+    describe('Model to validate match data', function () {
+
+        it('register model with match', function() {
+            this.vm.registerModel( "user_match", {
+                name: { type: "string", match : /^[A-Z]+$/ }
+            }).should.be.false;
+        });
+
+        it('check match data', function() {
+            this.vm.validate( "user_match", 
+                { name  : "ILIKECAPS" }
+            ).should.equal(false);
+        });    
+
+        it('check bad match data', function() {
+            this.vm.validate( "user_match", 
+                { name  : "ILIKEcAPS" }
+            ).should.eql({ '': 'Field "name" not matched with type "string"' });
+        });    
+
+    });
 
 
-  it('check bad match data', function() {
-    vm.validate( "user_match", 
-        { name  : "ILIKEcAPS" }
-    ).should.eql({ '': 'Field "name" not matched with type "string"' });
-  });    
+    describe('Model to validate integer data', function() {
+
+        it('register model with integer', function() {
+            this.vm.registerModel( "user_int", {
+                id:   { type: "integer" },
+            }).should.be.false;
+        });
+
+        it('check integer data', function() {
+            this.vm.validate( "user_int", 
+                { id : 123 }
+            ).should.equal(false);
+        });    
+
+        it('check bad integer data', function() {
+            this.vm.validate( "user_int", 
+                { id : 123.1 }
+            ).should.eql({ '': 'Field "id" not matched with type "integer"' });
+        });    
+
+    });
 
 
-  it('register model with integer and password', function() {
-    vm.registerModel( "user_int", {
-        id:   { type: "integer" },
-        pass: { type: "password" },
-    }).should.be.false;
-  });
+    describe('Model to validate password', function() {
+
+        it('register model with password', function() {
+            this.vm.registerModel( "user_pass", {
+                pass: { type: "password" },
+            }).should.be.false;
+        });
+
+        it('check password data', function() {
+            this.vm.validate( "user_pass", 
+                { pass : "R2d=" }
+            ).should.equal(false);
+        });    
+
+        it('check bad password data', function() {
+            this.vm.validate( "user_pass", 
+                { pass : "r2D2" }
+            ).should.eql({ "": "Field \"pass\" not matched with type \"password\"" });
+        });
+
+    });
 
 
-  it('check int and password data', function() {
-    vm.validate( "user_int", 
-        { id : 123, pass : "R2d=" }
-    ).should.equal(false);
-  });    
-
-
-  it('check bad int and password data', function() {
-    vm.validate( "user_int", 
-        { id : 123.1, pass : "r2D2" }
-    ).should.eql({ '': 'Field "id" not matched with type "integer"',
-            "": "Field \"pass\" not matched with type \"password\"" });
-  });    
-
-  it('register new bad model', function() {
-    vm.registerModel( ).should.equal("Name is not defined");
-  });
-
-
-  it('check bad data', function() {
-    vm.validate( "user", {
-      id    : "61cecfb4-da43-4b65-aaa0-f1c3be81ec53",
-      name  : { last: "Bagdanov", },
-      metadata: { tt1:1, tt2:2 },
-      createdAt : new Date(),
-    }).should.eql({ '': 'Field "createdAt" not found in registered model',
-      ',name': 'Field "first" is requied, but not found' });
-  });    
-
+    describe('Errors while registration', function() {
+        it('register new bad model', function() {
+            this.vm.registerModel( ).should.equal("Name is not defined");
+        });
+    });
     
 });
